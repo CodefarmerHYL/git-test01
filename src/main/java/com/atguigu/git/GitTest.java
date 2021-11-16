2a3a2e8543fea74240d7126cd31e8782c4f8ef32
CodefarmerHYL <92500954+CodefarmerHYL@users.noreply.github.com>
2021-11-16 20:22:54 +0800

    yueyue

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -7,15 +7,6 @@ package com.atguigu.git;
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello, git!");
-        System.out.println("hello, git2!");
-        System.out.println("hello, git3!");
-        System.out.println("hello, git4!");
-        System.out.println("master commit");
-        System.out.println("master commit2");
-        System.out.println("hot-fix commit3!");
-        System.out.println("hot-fix commit4!");
-        System.out.println("hot-fix commit5!");
-        System.out.println("hot-fix commit6!");
+        System.out.println("hello, git111!");
     }
 }
