54e77840028d4b030231418f4440e887c3a3cb69
HYL <1085068942@qq.com>
2021-11-16 20:17:33 +0800

    master 0.0.6

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -13,7 +13,8 @@ public class GitTest {
         System.out.println("hello, git4!");
         System.out.println("master commit");
         System.out.println("master commit2");
-        System.out.println("hot-fix commit!");
-        System.out.println("hot-fix commit!");
+        System.out.println("hot-fix commit3!");
+        System.out.println("hot-fix commit4!");
+        System.out.println("hot-fix commit5!");
     }
 }
