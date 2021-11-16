76d9236eb27d100e8d7558830aff89d82c5c8c9c
HYL <1085068942@qq.com>
2021-11-16 19:35:47 +0800

    IDEA 0.0.1

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @Description:
+ * @Author 胡菜鸡
+ * @Create 2021-11-16-19:32
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello, git!");
+    }
+}
